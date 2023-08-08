ff1e6b814c7492093ebbb42aae6df25344f304d8
Matthew <Matthew@Kathys-iMac.local>
2023-08-08 16:25:50 -0700

    update

--- puppyPals/src/App.jsx
+++ puppyPals/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <div className="App">
+          { 
+            puppies.map((puppy) => {
+              return <p>{puppy.name}</p>
+          })
+        }
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -23,11 +31,11 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
